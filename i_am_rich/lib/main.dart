6425614f792350e3a45e3abceddd8d181572930b
sergey_petrochenko <proffsneip@gmail.com>
2020-06-17 01:03:17 +0300

    updated image assets

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -11,8 +11,7 @@ void main() {
         backgroundColor: Colors.blueGrey,
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
