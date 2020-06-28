78b5e125a462ea1e790b18b57a275dac95613dc6
sergey_petrochenko <proffsneip@gmail.com>
2020-06-29 00:27:57 +0300

    implemented reusablecard widget

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -12,11 +12,54 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                ReusableCard(
+                  backgroundColor: Color(0xFF1D1E33),
+                ),
+                ReusableCard(
+                  backgroundColor: Color(0xFF1D1E33),
+                ),
+              ],
+            ),
+          ),
+          ReusableCard(
+            backgroundColor: Color(0xFF1D1E33),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                ReusableCard(
+                  backgroundColor: Color(0xFF1D1E33),
+                ),
+                ReusableCard(
+                  backgroundColor: Color(0xFF1D1E33),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color backgroundColor;
+
+  ReusableCard({@required this.backgroundColor});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
